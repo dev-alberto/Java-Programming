36d65da700f5ecd0d6ab8e221be04fe17678c8ff
Alberto Alexandru Cieri <alberto.cieri@info.uaic.ro>
2016-02-21 12:35:41 +0200

    Running time + size <3

--- PA_lab1/src/Euler.java
+++ PA_lab1/src/Euler.java
@@ -7,7 +7,18 @@ import java.lang.reflect.Array;
 import java.util.*;
 public class Euler {
     private static int size = 0;
-    private static char [][][] grecoLatinSquare = new char[3][3][2];
+    /*
+        grecoLatinSquare[i][j][0] is a latin letter
+        grecoLatinSquare[i][j][0] is a greek letter or number
+    */
+    private static final int  SIZE = 6;
+    private static char [][][] grecoLatinSquare = new char[SIZE][SIZE][2];
+    private static long numerOfGrecoLatinSquare = 0;
+    private static Boolean [][] latinLettersUsedForEachRow = new Boolean[SIZE][SIZE];
+    private static Boolean [][] latinLettersUsedForEachColum = new Boolean[SIZE][SIZE];
+    private static Boolean [][] grecoLettersUsedForEachRow = new Boolean[SIZE][SIZE];
+    private static Boolean [][] grecoLettersUsedForEachColum = new Boolean[SIZE][SIZE];
+    private static Boolean [][] latinGrecoPair = new Boolean[SIZE][SIZE];
 
     private static void circularPermutationToLeft(int line, int character) {
         char auxiliary = grecoLatinSquare[line - 1][0][character];
@@ -33,18 +44,84 @@ public class Euler {
 
     }
 
+    private static int get_colum(int h) {
+        return h % size;
+    }
+
+    private static int get_row(int h) {
+        return h / size;
+    }
+
+    private static void InitializeMatrixes() {
+        for (int i = 0; i < SIZE; ++i) {
+            for (int j = 0; j < SIZE; ++j) {
+                latinLettersUsedForEachRow[i][j] = false;
+                latinLettersUsedForEachColum[i][j] = false;
+                grecoLettersUsedForEachRow[i][j] = false;
+                grecoLettersUsedForEachColum[i][j] = false;
+                latinGrecoPair[i][j] = false;
+            }
+        }
+    }
+
+    private static void NumberGrecoLatinSquares(int h) {
+        if (h == size * size) {
+            ++numerOfGrecoLatinSquare;
+        }
+        else {
+            int row = get_row(h);
+            int colum = get_colum(h);
+            for (int latin = 0; latin < size; ++latin) {
+                if (!latinLettersUsedForEachRow[row][latin] && !latinLettersUsedForEachColum[colum][latin]) {
+
+                    latinLettersUsedForEachRow[row][latin] = true;
+                    latinLettersUsedForEachColum[colum][latin] = true;
+
+                    for (int greco = 0; greco < size; ++greco) {
+                        if (!grecoLettersUsedForEachRow[row][greco] && !grecoLettersUsedForEachColum[colum][greco]) {
+                            grecoLettersUsedForEachRow[row][greco] = true;
+                            grecoLettersUsedForEachColum[colum][greco] = true;
+
+                            if (!latinGrecoPair[latin][greco]) {
+                                latinGrecoPair[latin][greco] = true;
+                                NumberGrecoLatinSquares(h + 1);
+                                latinGrecoPair[latin][greco] = false;
+                            }
+
+                            grecoLettersUsedForEachRow[row][greco] = false;
+                            grecoLettersUsedForEachColum[colum][greco] = false;
+                        }
+                    }
+
+                    latinLettersUsedForEachRow[row][latin] = false;
+                    latinLettersUsedForEachColum[colum][latin] = false;
+                }
+            }
+        }
+    }
     public static void ConstructFirstLine(String [] args) {
         size = Integer.parseInt(args[0]);
+        if (size<3)
+        {
+            System.err.print("Nu exista patrate greco-romane de dimensiunea dorita");
+        }
         for (int j = 0; j <size; ++j) {
             grecoLatinSquare[0][j][0] = args[j + 1].charAt(0);
             grecoLatinSquare[0][j][1] = args[size + 1 + j].charAt(0);
         }
     }
 
+
     public static void main(String [] args) {
+        int start=(int) System.currentTimeMillis();
         ConstructFirstLine(args);
+        System.out.println("One greco-latin square of requested size: \n" );
         create();
         System.out.println(Arrays.deepToString(grecoLatinSquare));
-        StringBuilder a;
+        System.out.println("Trying to count here all of them, be patient...\n");
+        InitializeMatrixes();
+        NumberGrecoLatinSquares(0);
+        System.out.println(numerOfGrecoLatinSquare);
+        System.out.println("Running time= " + ((int) System.currentTimeMillis() - start) + "\n");
     }
 }
